803efb722363f03dad8f07672686a83a17ce3642
ndesc <n.desc64@gmail.com>
2020-01-22 16:14:48 +0100

    set font size dynamically

--- MelodyeuxApp/ClassVue/AccueilScene.java
+++ MelodyeuxApp/ClassVue/AccueilScene.java
@@ -20,15 +20,13 @@ public class AccueilScene extends WindowClass {
 
         //On définit tout les élément dont on a besoin et on les positionne
 
-        //hbox = new HBox();
-        quit = new Button("QUITTER");
-        //quit.setMinSize(1000, 200);
-        quit.setMinSize(WIDTH*0.5, HEIGHT*0.1);
 
-        //System.out.println(WIDTH + " : " + HEIGHT);
+        quit = new Button("QUITTER");
+        quit.setMinSize(WIDTH*0.5, HEIGHT*0.1);
 
         quit.setLayoutX((WIDTH-quit.getMinWidth())/2);
         quit.setLayoutY(HEIGHT*0.7);
+        quit.setStyle("-fx-font-size:" +  WIDTH*0.05);
         //On défini le style du bouton
         quit.setId("quitButton");
 
@@ -36,6 +34,7 @@ public class AccueilScene extends WindowClass {
         melodyeuxButton.setMinSize(WIDTH*0.75,HEIGHT*0.50);
         melodyeuxButton.setLayoutX((WIDTH-melodyeuxButton.getMinWidth())/2);
         melodyeuxButton.setLayoutY(HEIGHT*0.05);
+        melodyeuxButton.setStyle("-fx-font-size:" +  WIDTH*0.13);
         melodyeuxButton.setId("melodyeuxButton");
 
         //endregion
